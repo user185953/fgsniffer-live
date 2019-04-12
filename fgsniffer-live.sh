1b3b8f5ed72ad192eb8f2541e808ef67d19e485e
oasao <46689480+user185953@users.noreply.github.com>
2019-04-12 16:25:25 +0200

    shell wrapper for live capture

---
+++ fgsniffer-live.sh
@@ -0,0 +1,80 @@
+#!/bin/sh
+
+set -e
+
+CLInoMOREhack=' | grep ^'	# Choose if some of your units are configured config-system-console; set-output-more (FGT default)
+#CLInoMOREhack=''		# Choose if ALL your units are configured "set output normal". This may reduce latency.
+
+fgcommand_vdom='
+config vdom
+edit Prod'
+
+#################################
+
+host=""
+interface="any"
+userfilter=""
+mode=6
+count=0
+
+usage() {
+	echo "Usage: $0 [-n count] [-i interface|any] host [filter]"
+	exit $1
+}
+
+fgcommand_sniff="$fgcommand_vdom"'
+diag sniffer packet %s "%s" %s %s a'"$CLInoMOREhack"
+
+while getopts "hn:i:" opt; do
+	case "$opt" in
+		h)	usage 0 ;;
+		n)	count="$OPTARG" ;;
+		i)	interface="$OPTARG" ;;
+		*)	usage 1 ;;
+	esac
+done
+shift `expr $OPTIND - 1`
+
+case "$#" in
+	2)	host="$1"; userfilter="$2" ;;
+	1)	host="$1";;
+	0)	echo "Missing mandatory argument: Host" >&2
+		usage 1
+		;;
+	*)	echo "Too many arguments" >&2
+		usage 1
+		;;
+esac
+
+case "$host" in
+	# Fortigate 1-HA cluster
+	1a|fw1a)	hostIP=10.0.0.8  ;;
+	1b|fw1b)	hostIP=10.0.0.9  ;;
+	1|fw1)		hostIP=10.0.0.10 ;;
+	# Fortigate 2
+	2|fw2)		hostIP=10.1.0.10 ;;
+	# Fortigate 3
+	3|fw3)		hostIP=10.2.0.10 ;;
+	*)	cat <<END
+Unsupported host \"$host\", try one of:
+	UNCONFIGURED, edit the script
+	1	fw1	fw1a	fw1b
+	2	fw2
+	3	fw3
+END
+esac
+
+if ! [ 0 -le "$count" ]; then
+	echo "ERROR: count \"$count\" is not a number"
+	exit 1
+fi
+
+# Prevent us from capturing OUR OWN ssh connection
+sshfilter="(not (host $hostIP and port 22))"
+if [ -n "$userfilter" ]; then
+	filter="($userfilter) and ($sshfilter)"
+else
+	filter="$sshfilter"
+fi
+
+printf "$fgcommand_sniff" "$interface" "$filter" "$mode" "$count" | ssh "$hostIP" | sed 's/^[^#]\+ # *//' | fgsniffer-converter
